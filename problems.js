23c97445bc7a5d02be43fbb10c5f233aa83ea335
Michael Call <callmichael.bus@gmail.com>
2016-12-27 12:37:08 -0700

    jfkdlsa

--- problems.js
+++ problems.js
@@ -362,3 +362,27 @@ function elimination(array){
   return null
 }
 elimination(arr)
+// 13.
+// Your task is to return how many times a string contains a given character.
+//
+// The function takes a string(inputS) as a paremeter and a char(charS) which is the character that you will have to find and count.
+//
+// For example, if you get an input string "Hello world" and the character to find is "o", return 2.
+// *****************************************************************************************************************************************************************
+function stringCounter(inputS, charS){
+  inputs.split('')
+}
+// 14.
+// closures
+// *****************************************************************************************************************************************************************
+
+function closure() {
+ var num = 0
+
+   function addOne(){
+     return num + 1;
+   }
+ return {
+   add: addOne
+ }
+}
